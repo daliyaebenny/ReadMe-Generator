d3466554e785f338cd626ca199f234d092a88b73
Daliya Benny <daliyaebenny@gmail.com>
2021-10-15 00:40:04 -0700

    License added

--- index.js
+++ index.js
@@ -2,21 +2,48 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown");
-
+const path = require('path');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-    err ? console.log(err) : console.log('Successfully created Readme ')
-  );
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+    // fs.writeFile(fileName, data, (err) =>
+    // err ? console.log(err) : console.log('Successfully created Readme ')
+ // );
 }
 
+function fetchData() {
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your Project?',
+    },
+    {
+      type: 'list',
+      message: 'License',
+      name: 'license',
+      choices: ['MIT', 'ISC', 'APACHE 2.0', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What is the description of your Project?',
+    },
+  ])
+  .then((data) => {
+    console.log(data);
+    writeToFile('./result/README.md', generateMarkdown(data));
+
+  });
+}
 // TODO: Create a function to initialize app
 function init() {
-  console.log("Hello");
+  fetchData();
 }
 
 // Function call to initialize app
---
+++ result/README.md
@@ -0,0 +1,5 @@
+# ada
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg) 
+
+  ## Description 
+  kjk jhk 
